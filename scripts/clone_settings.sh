9ab16ac88546ea762f065f644ddaf440e1df1926
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-05-26 15:13:38 +0000

    chore: update clone settings script to clone .env

--- scripts/clone_settings.sh
+++ scripts/clone_settings.sh
@@ -2,16 +2,31 @@
 set -e
 
 # init settings.ini
-EXAMPLE_FILE="settings.example.ini"
-OUTPUT_FILE="settings.ini"
+EXAMPLE_SETTINGS_FILE="settings.example.ini"
+OUTPUT_SETTINGS_FILE="settings.ini"
 
-if [ ! -f "$EXAMPLE_FILE" ]; then
-    echo "Error: $EXAMPLE_FILE not found. Are you running this from the project root?"
+if [ ! -f "$EXAMPLE_SETTINGS_FILE" ]; then
+    echo "Error: $EXAMPLE_SETTINGS_FILE not found. Are you running this from the project root?"
     exit 1
 fi
 
-if [ -f "$OUTPUT_FILE" ]; then
-    echo "Warning: $OUTPUT_FILE already exists. Delete it first if you want to regenerate."
+if [ -f "$OUTPUT_SETTINGS_FILE" ]; then
+    echo "Warning: $OUTPUT_SETTINGS_FILE already exists. Delete it first if you want to regenerate."
 else
-  cp "$EXAMPLE_FILE" "$OUTPUT_FILE"
+  cp "$EXAMPLE_SETTINGS_FILE" "$OUTPUT_SETTINGS_FILE"
+fi
+
+# init .env
+EXAMPLE_ENV_FILE=".env.example"
+OUTPUT_ENV_FILE=".env"
+
+if [ ! -f "$EXAMPLE_ENV_FILE" ]; then
+    echo "Error: $EXAMPLE_ENV_FILE not found. Are you running this from the project root?"
+    exit 1
+fi
+
+if [ -f "$OUTPUT_ENV_FILE" ]; then
+    echo "Warning: $OUTPUT_ENV_FILE already exists. Delete it first if you want to regenerate."
+else
+  cp "$EXAMPLE_ENV_FILE" "$OUTPUT_ENV_FILE"
 fi
