3da57b41fbd93059f2193f12c1f771e0820fc8a1
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-05-26 16:25:35 +0000

    feat: add back logging system

--- core/settings.py
+++ core/settings.py
@@ -206,3 +206,21 @@ EMAIL_BACKEND = (
     if DEBUG
     else "django.core.mail.backends.smtp.EmailBackend"
 )
+
+# ─────────────────────────────────────────────────────────────────| Logging |──
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "console": {"class": "logging.StreamHandler"},
+    },
+    "loggers": {
+        "django": {"handlers": ["console"], "level": "ERROR"},
+        "django.request": {
+            "handlers": ["console"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
