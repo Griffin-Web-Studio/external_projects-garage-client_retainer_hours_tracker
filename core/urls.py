391205888e1142086f09fa3f8e3507cbdcb9301c
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-05-26 16:26:49 +0000

    refactor: update url patterns in core and tracker app

--- core/urls.py
+++ core/urls.py
@@ -16,8 +16,20 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.auth.views import LogoutView
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(url="/dashboard/", permanent=False)),
     path("admin/", admin.site.urls),
+    path("oidc/", include("mozilla_django_oidc.urls")),
+    path("logout/", LogoutView.as_view(), name="logout"),
+    path("", include("tracker.urls")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [path("__reload__/", include("django_browser_reload.urls"))]
+
+# handler404 = "tracker.views.page_not_found.page_not_found"
