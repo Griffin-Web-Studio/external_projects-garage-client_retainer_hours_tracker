2436240360057d5d14ee8e65acef6dbd7e9f73d5
Rihards Simanovics <rihards.s@griffin-web.studio>
2026-05-26 16:22:55 +0000

    docs: correct version for the url

--- core/settings.py
+++ core/settings.py
@@ -110,7 +110,7 @@ DATABASES = {
 }
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+# https://docs.djangoproject.com/en/6.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
